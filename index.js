92382a9802b39d2c9b21b109d758b9fb031c0f90
Archita <asa410@sfu.ca>
2022-12-24 02:02:12 -0800

    Created a function to make the buttons seem animated

--- index.js
+++ index.js
@@ -42,24 +42,34 @@ function sound(input){
             audio = new Audio("sounds/kick-bass.mp3");
             break;
         default:
-            alert("this.innerHTML");
+            
             break;
     }
 }
 
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    },150);
+}
 
 for (var i = 0 ; i < length ; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+        
         sound(this.innerHTML);
         audio.play();
+        buttonAnimation(this.innerHTML);
+        
     });
 }
 
 
 document.addEventListener("keydown", function(event){
-    console.log(event);
     sound(event.key);
     audio.play();
+    buttonAnimation(event.key);
 });
     
 
@@ -67,32 +77,3 @@ document.addEventListener("keydown", function(event){
 
 
 
-
-
-// console.log(this.innerHTML)
-        // switch (this.innerHTML) {
-        //     case "w":
-        //         audio = new Audio("sounds/tom-2.mp3");
-        //         break;
-        //     case "a":
-        //         audio = new Audio("sounds/tom-1.mp3");
-        //         break;
-        //     case "s":
-        //         audio = new Audio("sounds/tom-3.mp3");
-        //         break;
-        //     case "d":
-        //         audio = new Audio("sounds/tom-4.mp3");
-        //         break;
-        //     case "j":
-        //         audio = new Audio("sounds/crash.mp3");
-        //         break;
-        //     case "k":
-        //         audio = new Audio("sounds/snare.mp3");
-        //         break;
-        //     case "l":
-        //         audio = new Audio("sounds/kick-bass.mp3");
-        //         break;
-        //     default:
-        //         alert("this.innerHTML");
-        //         break;
-        // }
